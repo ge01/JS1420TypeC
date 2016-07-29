36493e30458e96b40f3bb70fbb3f54ce3a9c7e93
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-28 20:42:45 -0500

    unary operator

---
+++ app.js
@@ -0,0 +1,8 @@
+/****************************************************
+*               The Unary + Operator                *
+****************************************************/
+function myFunction() {
+  var y = "5";
+  var x = + y;
+  document.getElementById("unary-op").innerHTML = typeof y + "<br>" + typeof x;
+}

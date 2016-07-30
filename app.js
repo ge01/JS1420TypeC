45992bc376d4f10905f2f5a7839da77fac89b59e
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-29 22:57:56 -0500

    type conversion

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ function myFunction() {
   var x = + y;
   document.getElementById("unary-op").innerHTML = typeof y + "<br>" + typeof x;
 }
+
+/****************************************************
+*              Automatic Type Conversion            *
+****************************************************/
+var x = [];
+document.getElementById("automatic").innerHTML =
+(5 + null) + "<br>"  +
+("5" + null) + "<br>" +
+("5" + 2) + "<br>" +
+("5" - 2) + "<br>" +
+("5" * "2") + "<br>" +
+("5" / "2") + "<br>";
+
+/****************************************************
+*           JavaScript Type Conversion Table        *
+****************************************************/
+var x = false;
+document.getElementById("false-conv").innerHTML =
+"Number : " + Number(x) + "<br>" +
+"String : " + String(x) + "<br>" +
+"Boolean: " + Boolean(x);
